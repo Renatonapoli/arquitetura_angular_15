f7d3e3e15af4bc15ac21225fc62693bde1fc1d62
Renato-napoli <renato.napoli75@gmail.com>
2025-01-25 18:20:23 -0300

    Recebendo e Enviando dados do formulário

--- src/app/components/formularios/formulario/formulario.component.ts
+++ src/app/components/formularios/formulario/formulario.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css'],
 })
-export class FormularioComponent {}
+export class FormularioComponent {
+  @Input() dadosFormulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.dadosFormulario = this.fb.group({
+      dadosPessoais: fb.group({
+        nome: [null],
+        email: [null],
+        profissao: [null],
+        idade: [null],
+        sexo: [null],
+      }),
+      endereco: fb.group({
+        cep: [null],
+        rua: [null],
+        complemento: [null],
+        numero: [null],
+        estado: [null],
+        cidade: [null],
+      }),
+    });
+  }
+
+  salvarDadosFormulario() {
+    console.log(this.dadosFormulario.value);
+  }
+}
--- src/app/components/formularios/formularios.module.ts
+++ src/app/components/formularios/formularios.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { LogFormularioComponent } from './formulario/log-formulario/log-formulario.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormularioComponent, LogFormularioComponent],
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   exports: [FormularioComponent],
 })
 export class FormulariosModule {}
